7801b447f716fd66808f495dedef8edd80b7250e
Rafael T. C. Soares <rafaelcba@gmail.com>
2018-04-17 16:16:17 -0300

    workaround for patterfly tests pages

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -17,13 +17,16 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
 hugo
+
+echo "Removing patternfly test pages due some GitHub pages restrictions/issues..."
+rm -rf themes/redhatgov/static/node_modules/patternfly/tests
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
